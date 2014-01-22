045db3af1b67674cf3e85157e12ae790c3e29df2
Chromator <kulkarni.swaroops@gmail.com>
2014-01-22 17:55:09 +0530

    Working example for inheritance in JS and added readme.

---
+++ objects/Inheritance_in_JS.js
@@ -0,0 +1,32 @@
+function Person(firstname, lastname, age, eyecolor) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = age;
+    this.eyecolor = eyecolor;
+}
+Person.prototype.name = function() {
+    return this.firstname + " " + this.lastname;
+}
+
+Then make the .prototype object of Student an instance of Person:
+
+function Student(firstname, lastname, age, eyecolor, level) {
+    this.level = level;
+    Person.call(this, firstname, lastname, age, eyecolor);
+}
+
+// Make the prototype object of the Student constructor inherit from the
+//    prototype object of the Person constructor.
+Student.prototype = Object.create(Person.prototype)
+
+And so now your name method is shared among all instances created instead of being remade for each instance.
+
+var per = new Person("Abe", "Lincoln", 45, "green");
+
+var obj = new Student("Abe", "Lincoln", 45, "green", "senior");
+
+
+
+
+Stackoverflow link:
+http://stackoverflow.com/questions/14915408/javascript-parent-child-inheritance-without-using-prototype
---
+++ objects/Readme.txt
@@ -0,0 +1,10 @@
+Object creation in Javascript
+
+car.js : simple example of inheritance
+car2.js : Object creation as associative array
+car3.js : class methods
+eg1 : Examples above run using this html
+eg2a : dynamically adding members to an object
+Prg3a, Prg3b : Use of prototype for inheritance
+eg3a : Object creation similar to associative array
+Inheritance_in_JS.js : Example of inheriting member variables in JS
